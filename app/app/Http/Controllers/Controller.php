4f25b622ff58d92b41181d196ba59c637e04bab5
Martin Erpicum <martin@erpicum.net>
2022-11-27 06:22:30 +0100

    📜 openapi docs

--- app/app/Http/Controllers/Controller.php
+++ app/app/Http/Controllers/Controller.php
@@ -7,6 +7,32 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+     * @OA\Info(
+     *      version="1.0.0",
+     *      title="BE Companies API Documentation",
+     *      description="OpenApi description of the BE Companies API",
+     *      @OA\Contact(
+     *          email="martin@erpicum.net"
+     *      ),
+     *      @OA\License(
+     *          name="GPL-3.0 License",
+     *         url="https://www.gnu.org/licenses/gpl-3.0.en.html"
+     *      )
+     * )
+     *
+     * @OA\Server(
+     *      url=L5_SWAGGER_CONST_HOST,
+     *      description="BE Companies API Server"
+     * )
+
+     *
+     * @OA\Tag(
+     *     name="Projects",
+     *     description="API Endpoints of Projects"
+     * )
+     */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
--- app/app/Http/Resources/BaseResource.php
+++ app/app/Http/Resources/BaseResource.php
@@ -18,10 +18,8 @@ class BaseResource extends JsonResource
                 
                 'api_version' => 'v1',
                 'api_base' => route('api.index'),
-                'api_documentation' => [
-                    'swagger' => route('api.index') . '/api-docs.html',
-                    'redoc' => route('api.index') . '/api-redoc.html',
-                ],
+                // api/documentation to base + api/documentation
+                'api_documentation' => route('api.index') . '/documentation',
                 'source_code' => env('SOURCE_CODE'),
                 'author' => env('AUTHOR_NAME'),
                 'disclaimer' => '⚠️ What you are seeing is raw technical data formatted in a non human readable way. It is not meant to be user-friendly.',
